cec61f7c1dc334e1011fe80a2db56f2c34d6d0ce
Ashton Shane <ashton.shane.dev@gmail.com>
2025-11-23 21:44:24 +0800

    implement form field validation for registration

--- flaskr/helpers.py
+++ flaskr/helpers.py
@@ -1,3 +1,23 @@
+import re
+
+def validate_field(input, type):
+    pattern = None
+
+    # Switchboard to handle different pattern types
+    match type:
+        case "email": # e.g. "aaa@gmail.com"
+            pattern = r"/^[a-zA-Z0–9._%+-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,}$/"
+        case "pw": # One special character, one capital letter, one number, min. 8 characters
+            pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$"
+        case "user": # min6 max14 alphanumeric chars. No underscores or periods
+            pattern = r"[A-Za-z0-9]{6,14}"
+    
+    #regex validation using fullmatch
+    if re.fullmatch(pattern, type):
+        return True
+    
+    return False
+
 def register_user(get_db, username, email, pw_hash, name):
     conn = get_db()
     cur = conn.cursor()
