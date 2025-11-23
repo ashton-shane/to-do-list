546b915b8042b73a8c3479f91caed7597c8d30de
Ashton Shane <ashton.shane.dev@gmail.com>
2025-11-23 19:19:10 +0800

    Implement database connection and functionality (#7)
    * add sqlite3 boilerplate
    
    * add query_db() helper function and populate register_user function with SQL query
    
    * fix import from helpers.py
    
    * fix database connection and register_user function

--- flaskr/helpers.py
+++ flaskr/helpers.py
@@ -1,1 +1,14 @@
-def register_user(name, username, pw_hash):
+def register_user(get_db, username, email, pw_hash, name):
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO users (name, username, email, password_hash) VALUES (?,?,?,?)", 
+                (name, username, email, pw_hash))
+    conn.commit()
+    cur.close()
+
+# === DB Helper === #
+def query_db(get_db, query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
